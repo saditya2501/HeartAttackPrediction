75457db82a13a0ff7d065e117adb93b93ff0d0c1
Aditya Singh Chauhan <aditya.chauhan_cs18@gla.ac.in>
2021-12-13 16:50:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ from flask import Flask, request, render_template
 model = pickle.load(open('heart_disease.pkl', 'rb')) 
 
 # Create application
-app = Flask(__name__)
+app = Flask(__name__,template_folder='templates')
 
 # Bind home function to URL
 @app.route('/')
@@ -58,4 +58,4 @@ def predict():
 if __name__ == '__main__':
 #Run the application
     port = os.environ.get("PORT", 5000)
-    app.run(debug = False,host = "0.0.0.0",port=port )
+    app.run(debug = False,host = "0.0.0.0",port=port)
